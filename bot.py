0bb886b3834f1cc2acf34443e6194748835f4cbc
Jaxx2712 <jgonzalezrodriguez652@gmail.com>
2025-09-07 11:23:59 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,58 @@
+import discord
+import random
+from discord.ext import commands
+from settings import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def saludo(ctx):
+    await ctx.send("Hola, bienvenido a mi servidor!")
+
+@bot.command()
+async def saludo_nombre(ctx, nombre:str):
+    await ctx.send(f"Hola, {nombre} bienvenido a mi servidor!")
+
+@bot.command()
+async def suma(ctx, numero1:int, numero2:int):
+    await ctx.send(f"La suma de {numero1} + {numero2} es {numero1 + numero2}")
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+bot.run(token)
